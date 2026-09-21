d0e422983744cc99b55e85758a8069db98a5485e
Aditi <atharvathakre.2907@gmail.com>
2026-09-21 18:18:59 +0530

    Add simple JS API test

---
+++ api/hello.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json({ 
+    ok: true,
+    message: 'Hello from API!',
+    timestamp: new Date().toISOString()
+  });
+}
